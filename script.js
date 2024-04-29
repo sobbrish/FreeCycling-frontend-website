dec739d86d47e3c03deee282e88cf38ebabde47b
sobbrish <AVeryTallPerson3m@gmail.com>
2024-04-30 11:59:56 +1200

    assignment requirements

---
+++ script.js
@@ -0,0 +1,8 @@
+const modal = document.getElementById("modal");
+
+function openModal(){
+    modal.classList.add("open-Modal");
+}
+function closeModal(){
+    modal.classList.remove("open-Modal");
+}
